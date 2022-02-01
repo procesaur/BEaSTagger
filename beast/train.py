fb634537f150e5f4c7f5a1fc96b20ca95018789a
procesaur <31268468+procesaur@users.noreply.github.com>
2022-02-01 21:09:13 +0100

    Update train.py

--- beast/train.py
+++ beast/train.py
@@ -8,7 +8,7 @@ from tkinter import Tk, filedialog as fd
 
 
 def train(file_path="", out_path="", pretrained=False, test_ratio=0.9, tune_ratio=0.9,
-          lexiconmagic=True, transliterate=False, lexicons_path="", beast_dir="",
+          lexiconmagic=True, transliterate=False, lexicons_path="", beast_dir="newBEaST",
           tt_path="", lex_paths={}, oc_paths={}, tunepaths={}, testing=False, onlytesting="",
           fulltest=False, epochs=120, batch_size=32, learning_rate=0.001, confidence=0.92, transfer=False):
 
@@ -45,8 +45,6 @@ def train(file_path="", out_path="", pretrained=False, test_ratio=0.9, tune_rati
     #initiate paths
     if lexicons_path == "":
         lexicons_path = path.join(path.dirname(__file__), "data/lexicon/")
-    if beast_dir == "":
-        beast_dir = path.join(path.dirname(__file__), "data/output/newBEaST")
     if tt_path == "":
         tt_path = path.join(path.dirname(__file__), "TreeTagger/bin/")
     
