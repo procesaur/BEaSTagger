8409325b05c59229ddde58de0b4ad976d7d81980
procesaur <31268468+procesaur@users.noreply.github.com>
2022-02-01 21:30:44 +0100

    Update train.py

--- beast/train.py
+++ beast/train.py
@@ -7,7 +7,7 @@ from beast.scripts.pipeline import training_prep, ratio_split
 from tkinter import Tk, filedialog as fd
 
 
-def train(file_path="", out_path="", pretrained=False, test_ratio=0.9, tune_ratio=0.9,
+def train(file_path="", out_path="temp", pretrained=False, test_ratio=0.9, tune_ratio=0.9,
           lexiconmagic=True, transliterate=False, lexicons_path="", beast_dir="newBEaST",
           tt_path="", lex_paths={}, oc_paths={}, tunepaths={}, testing=False, onlytesting="",
           fulltest=False, epochs=120, batch_size=32, learning_rate=0.001, confidence=0.92, transfer=False):
