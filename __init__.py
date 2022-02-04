118e3af334dab55d9bf22f219d02b906628d7caa
Mihailo Škorić <31268468+procesaur@users.noreply.github.com>
2022-02-04 15:44:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,2 @@
 from beast.train import train
 from beast.tag import tag
-
-
-#train(file_path="beast/data/training/SrpKor4Tagging", test_ratio=0.9)
-train(onlytesting="testing")
-#train(onlytesting="./data/output/testing", fulltest=True)
-#tag(model="./data/output/final", transliterate=True)
