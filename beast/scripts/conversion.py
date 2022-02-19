b023c7c090b9810a5264279614f870f6aebeca0c
procesaur <31268468+procesaur@users.noreply.github.com>
2022-02-19 08:28:16 +0100

    Update conversion.py

--- beast/scripts/conversion.py
+++ beast/scripts/conversion.py
@@ -1,6 +1,4 @@
-def convert(word, ctype, direction=""):
-    if ctype == 'LATtoASC':
-        dic = {
+lat2asc = {
             'Dž': 'Dy',
             'Lj': 'Lx',
             'Nj': 'Nx',
@@ -21,30 +19,7 @@ def convert(word, ctype, direction=""):
             'đ': 'dx',
             'š': 'sx'
         }
-    if ctype == 'ASCtoLAT':
-        dic = {
-            'Dy': 'Dž',
-            'Lx': 'Lj',
-            'Nx': 'Nj',
-            'DY': 'DŽ',
-            'LX': 'LJ',
-            'NX': 'NJ',
-            'lx': 'lj',
-            'nx': 'nj',
-            'dy': 'dž',
-            'Cy': 'Č',
-            'Cx': 'Ć',
-            'Zx': 'Ž',
-            'Dx': 'Đ',
-            'Sx': 'Š',
-            'cy': 'č',
-            'cx': 'ć',
-            'zx': 'ž',
-            'dx': 'đ',
-            'sx': 'š'
-        }
-    if ctype == 'LATtoCYR':
-        dic = {
+lat2cyr = {
             'LJ': 'Љ',
             'NJ': 'Њ',
             'DŽ': 'Џ',
@@ -109,71 +84,7 @@ def convert(word, ctype, direction=""):
             'č': 'ч',
             'š': 'ш'
         }
-    if ctype == 'CYRtoLAT':
-        dic = {
-            'Љ': 'Lj',
-            'Њ': 'Nj',
-            'Џ': 'Dž',
-            'А': 'A',
-            'Б': 'B',
-            'В': 'V',
-            'Г': 'G',
-            'Д': 'D',
-            'Ђ': 'Đ',
-            'Е': 'E',
-            'Ж': 'Ž',
-            'З': 'Z',
-            'И': 'I',
-            'Ј': 'J',
-            'К': 'K',
-            'Л': 'L',
-            'М': 'M',
-            'Н': 'N',
-            'О': 'O',
-            'П': 'P',
-            'Р': 'R',
-            'С': 'S',
-            'Т': 'T',
-            'Ћ': 'Ć',
-            'У': 'U',
-            'Ф': 'F',
-            'Х': 'H',
-            'Ц': 'C',
-            'Ч': 'Č',
-            'Ш': 'Š',
-            'љ': 'lj',
-            'њ': 'nj',
-            'џ': 'dž',
-            'а': 'a',
-            'б': 'b',
-            'в': 'v',
-            'г': 'g',
-            'д': 'd',
-            'ђ': 'đ',
-            'е': 'e',
-            'ж': 'ž',
-            'з': 'z',
-            'и': 'i',
-            'ј': 'j',
-            'к': 'k',
-            'л': 'l',
-            'м': 'm',
-            'н': 'n',
-            'о': 'o',
-            'п': 'p',
-            'р': 'r',
-            'с': 's',
-            'т': 't',
-            'ћ': 'ć',
-            'у': 'u',
-            'ф': 'f',
-            'х': 'h',
-            'ц': 'c',
-            'ч': 'č',
-            'ш': 'š'
-        }
-    if ctype == 'CYRtoASC':
-        dic = {
+cyr2asc = {
             'Љ': 'Lx',
             'Њ': 'Nx',
             'Џ': 'Dy',
@@ -235,78 +146,31 @@ def convert(word, ctype, direction=""):
             'ч': 'cy',
             'ш': 'sx'
         }
+asc2lat = {v: k for k, v in lat2asc.items()}
+cyr2lat = {v: k for k, v in lat2cyr.items()}
+asc2cyr = {v: k for k, v in cyr2asc.items()}
+
+
+def convert(text, ctype, direction=""):
+    dic = {}
+    if ctype == 'LATtoASC':
+        dic = lat2asc
+    if ctype == 'ASCtoLAT':
+        dic = asc2lat
+    if ctype == 'LATtoCYR':
+        dic = lat2cyr
+    if ctype == 'CYRtoLAT':
+        dic = cyr2lat
+    if ctype == 'CYRtoASC':
+        dic = cyr2asc
     if ctype == 'ASCtoCYR':
-        dic = {
-            'LX': 'Љ',
-            'NX': 'Њ',
-            'DY': 'Џ',
-            'Lx': 'Љ',
-            'Nx': 'Њ',
-            'Dy': 'Џ',
-            'Dx': 'Ђ',
-            'Zx': 'Ж',
-            'Cx': 'Ћ',
-            'Cy': 'Ч',
-            'Sx': 'Ш',
-            'A': 'А',
-            'B': 'Б',
-            'V': 'В',
-            'G': 'Г',
-            'D': 'Д',
-            'E': 'Е',
-            'Z': 'З',
-            'I': 'И',
-            'J': 'Ј',
-            'K': 'К',
-            'L': 'Л',
-            'M': 'М',
-            'N': 'Н',
-            'O': 'О',
-            'P': 'П',
-            'R': 'Р',
-            'S': 'С',
-            'T': 'Т',
-            'U': 'У',
-            'F': 'Ф',
-            'H': 'Х',
-            'C': 'Ц',
-            'lx': 'љ',
-            'nx': 'њ',
-            'dy': 'џ',
-            'dx': 'ђ',
-            'zx': 'ж',
-            'cx': 'ћ',
-            'cy': 'ч',
-            'sx': 'ш',
-            'a': 'а',
-            'b': 'б',
-            'v': 'в',
-            'g': 'г',
-            'd': 'д',
-            'e': 'е',
-            'z': 'з',
-            'i': 'и',
-            'j': 'ј',
-            'k': 'к',
-            'l': 'л',
-            'm': 'м',
-            'n': 'н',
-            'o': 'о',
-            'p': 'п',
-            'r': 'р',
-            's': 'с',
-            't': 'т',
-            'u': 'у',
-            'f': 'ф',
-            'h': 'х',
-            'c': 'ц'
-        }
+        dic = asc2cyr
 
     if direction == 'back':
         for key in dic.keys():
-            word = word.replace(dic[key], key)
+            text = text.replace(dic[key], key)
     else:
         for key in dic.keys():
-            word = word.replace(key, dic[key])
+            text = text.replace(key, dic[key])
 
-    return word
+    return text
