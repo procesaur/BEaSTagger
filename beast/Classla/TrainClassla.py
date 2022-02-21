8d2a2543ae0cf1a71bc963439fe88e19fcf3a584
procesaur <31268468+procesaur@users.noreply.github.com>
2022-02-21 13:14:27 +0100

    stanza training

---
+++ beast/Classla/TrainClassla.py
@@ -0,0 +1,91 @@
+import os
+import argparse
+from classla.models.tagger import train
+from classla.models.common import utils
+import torch
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='data/pos', help='Root dir for saving models.')
+    parser.add_argument('--wordvec_dir', type=str, default='extern_data/word2vec', help='Directory of word vectors.')
+    parser.add_argument('--wordvec_file', type=str, default=None, help='Word vectors filename.')
+    parser.add_argument('--train_file', type=str, default=None, help='Input file for data loader.')
+    parser.add_argument('--eval_file', type=str, default=None, help='Input file for data loader.')
+    parser.add_argument('--output_file', type=str, default=None, help='Output CoNLL-U file.')
+    parser.add_argument('--gold_file', type=str, default=None, help='Output CoNLL-U file.')
+    parser.add_argument('--pretrain_file', type=str, default=None, help='Input file containing pretrained data.')
+    parser.add_argument('--constrain_via_lexicon', type=str, default=None, help="Input location of lemmatization model.")
+
+    parser.add_argument('--mode', default='train', choices=['train', 'predict'])
+    parser.add_argument('--lang', type=str, help='Language')
+    parser.add_argument('--shorthand', type=str, help="Treebank shorthand")
+
+    parser.add_argument('--hidden_dim', type=int, default=200)
+    parser.add_argument('--char_hidden_dim', type=int, default=400)
+    parser.add_argument('--deep_biaff_hidden_dim', type=int, default=400)
+    parser.add_argument('--composite_deep_biaff_hidden_dim', type=int, default=100)
+    parser.add_argument('--word_emb_dim', type=int, default=75)
+    parser.add_argument('--char_emb_dim', type=int, default=100)
+    parser.add_argument('--tag_emb_dim', type=int, default=50)
+    parser.add_argument('--transformed_dim', type=int, default=125)
+    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--char_num_layers', type=int, default=1)
+    parser.add_argument('--pretrain_max_vocab', type=int, default=250000)
+    parser.add_argument('--word_dropout', type=float, default=0.33)
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--rec_dropout', type=float, default=0, help="Recurrent dropout")
+    parser.add_argument('--char_rec_dropout', type=float, default=0, help="Recurrent dropout")
+    parser.add_argument('--no_char', dest='char', action='store_false', help="Turn off character model.")
+    parser.add_argument('--no_pretrain', dest='pretrain', action='store_false', help="Turn off pretrained embeddings.")
+    parser.add_argument('--share_hid', action='store_true', help="Share hidden representations for UPOS, XPOS and UFeats.")
+    parser.set_defaults(share_hid=False)
+
+    parser.add_argument('--sample_train', type=float, default=1.0, help='Subsample training data.')
+    parser.add_argument('--optim', type=str, default='adam', help='sgd, adagrad, adam or adamax.')
+    parser.add_argument('--lr', type=float, default=3e-3, help='Learning rate')
+    parser.add_argument('--beta2', type=float, default=0.95)
+
+    parser.add_argument('--max_steps', type=int, default=50000)
+    parser.add_argument('--eval_interval', type=int, default=100)
+    parser.add_argument('--fix_eval_interval', dest='adapt_eval_interval', action='store_false', \
+            help="Use fixed evaluation interval for all treebanks, otherwise by default the interval will be increased for larger treebanks.")
+    parser.add_argument('--max_steps_before_stop', type=int, default=3000)
+    parser.add_argument('--batch_size', type=int, default=5000)
+    parser.add_argument('--max_grad_norm', type=float, default=1.0, help='Gradient clipping.')
+    parser.add_argument('--log_step', type=int, default=20, help='Print log every k steps.')
+    parser.add_argument('--save_dir', type=str, default='saved_models/pos', help='Root dir for saving models.')
+    parser.add_argument('--save_name', type=str, default=None, help="File name to save the model")
+
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--cuda', type=bool, default=torch.cuda.is_available())
+    parser.add_argument('--cpu', action='store_true', help='Ignore CUDA.')
+    parser.add_argument('--lemma_pretag', type=bool, default=False)
+    args = parser.parse_args(args=args)
+    return args
+
+
+def train_stanza(train_file, dev_in_file, out, pretrain=None):
+    batch_size = "30"
+
+    train_args = ["--wordvec_dir", None,
+                  "--wordvec_file", pretrain,
+                  "--train_file", train_file,
+                  "--eval_file", dev_in_file,
+                  "--output_file", out,
+                  "--gold_file", out+'.gold',
+                  "--batch_size", batch_size,
+                  "--lang", "sr",
+                  "--shorthand", "sr_set",
+                  "--pretrain_file", pretrain,
+                  "--mode", "train"]
+    train_args = train_args #+ ["--no_pretrain"]
+
+    args = parse_args(train_args)
+
+    if args.cpu:
+        args.cuda = False
+    utils.set_random_seed(args.seed, args.cuda)
+
+    args = vars(args)
+    train(args)
